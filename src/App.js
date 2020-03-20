c3e6435d5355f41ab7d12934ee9674f5763d3092
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-03-20 19:29:13 -0400

    Render the Welcome Page and made Axios call in the ChracterList.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
-    <main data-testid='app'>
+    <main data-testid="app">
       <Header />
+      <Route exact path="/" component={WelcomePage} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,27 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
+  const [character, setcharacter] = useState([]);
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacters = () => {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`
+        )
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+
+    getCharacters();
   }, []);
 
   return (
